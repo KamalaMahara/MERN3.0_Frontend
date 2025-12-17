37563d169d62a92f025e814c68ec94fb9414f94c
Kamala Mahara <kmlamahara@gmail.com>
2025-12-17 23:49:38 +0545

    Refactor Home and Cards components to fetch and display blog data dynamically; update routing in App component and remove unnecessary StrictMode in main entry.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
+        <Route path="" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
