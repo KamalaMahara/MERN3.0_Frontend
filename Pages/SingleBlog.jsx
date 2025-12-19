dc779bd6602e3746f7df234954205abc58fdb403
Kamala Mahara <kmlamahara@gmail.com>
2025-12-19 20:53:12 +0545

    Fetch and display individual blog details; implement delete functionality in SingleBlog component

--- Pages/SingleBlog.jsx
+++ Pages/SingleBlog.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const SingleBlog = () => {
+  const { _id } = useParams();
+  const navigate = useNavigate();
+  const [blog, setBlogs] = useState({});
+
+  const fetchSingleBlog = async () => {
+    const response = await axios.get(`http://localhost:3000/blog/${_id}`);
+    setBlogs(response.data.data);
+    console.log(response);
+  }
+
+  useEffect(() => {
+    fetchSingleBlog();
+  }, [])
+  const deleteBlog = async () => {
+    const response = await axios.delete(`http://localhost:3000/blog/${_id}`)
+    console.log(response.status);
+    if (response.status === 200) {
+      alert("Blog Deleted Successfully");
+      navigate("/");
+
+    }
+    else {
+      alert(" kuch to gadbad hai ");
+    }
+  }
+
   return (
     <>
       <Navbar />
@@ -10,31 +38,25 @@ const SingleBlog = () => {
           <div className="flex flex-col md:flex-row -mx-4">
             <div className="md:flex-1 px-4">
               <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                <img className="w-full h-full object-cover" src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg" alt="Product Image" />
+                <img className="w-full h-full object-cover" src={`http://localhost:3000/${blog.image}`} />
               </div>
               <div className="flex -mx-2 mb-4">
-                <div classNameName="w-1/2 px-2">
+                <div className="w-1/2 px-2">
                   <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit Blog</button>
                 </div>
                 <div className="w-1/3 px-2">
-                  <button className="w-full bg-gray-200 dark:bg-red-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Delete Blog</button>
+                  <button className="w-full bg-gray-200 dark:bg-red-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600 " onClick={deleteBlog}>Delete Blog</button>
                 </div>
               </div>
             </div>
             <div className="md:flex-1 px-4">
-              <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">Blog title</h2>
+              <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{blog.title}</h2>
               <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-                ante justo. Integer euismod libero id mauris malesuada tincidunt.
+                {blog.subtitle}
               </p>
 
-              <span className="font-bold text-gray-700 dark:text-gray-300">Blog Description:</span>
               <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla ut
-                lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit. Quisque
-                ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia, non
-                sagittis mauris blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt mi consectetur.
+                {blog.description}
               </p>
             </div>
           </div>
